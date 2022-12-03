3db660f022c1b0f9b742fbd10c6aeeb6dd61f35b
Ravikiran Reddy Balemla <56499420+kirannani168@users.noreply.github.com>
2022-12-03 18:56:16 +0530

    Styled ‘Header’ (External Styling)

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,17 +1,17 @@
 import React from "react";
-
+import '../Styles/Header.css'
 const Header = function(){
-    const headerStyle = {
-        textAlign: "center",
-        backgroundColor: "black",
-        color: "white",
-        padding: 20,
-        textTransform: 'uppercase',
-        fontSize:24,
-        fontWeight: "bolder"
-    }
+    // const headerStyle = {
+    //     textAlign: "center",
+    //     backgroundColor: "black",
+    //     color: "white",
+    //     padding: 20,
+    //     textTransform: 'uppercase',
+    //     fontSize:24,
+    //     fontWeight: "bolder"
+    // }
     return(
-        <div className="header" style={headerStyle}>
+        <div className="header">
             Header
         </div>
     )
