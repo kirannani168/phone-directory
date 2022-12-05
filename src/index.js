ea34dc6e58e9e3789799d91aa60320d00f57509a
Ravikiran Reddy Balemla <56499420+kirannani168@users.noreply.github.com>
2022-12-06 05:16:18 +0530

    Rendered "AddSubscriber" Component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import './Styles/Common.css';
+
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import AddSubscriber from './Components/AddSubscriber';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AddSubscriber />
   </React.StrictMode>
 );
 
